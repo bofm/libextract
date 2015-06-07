858cd556ef1e317424cd9de8538ecc091b3f68b7
bofm <sergeypostman@gmail.com>
2015-06-07 22:26:27 +0300

    extract() returns namedtuple('Extracted', 'nodes tree')

--- libextract/api.py
+++ libextract/api.py
@@ -1,15 +1,19 @@
 from functools import partial
+from collections import namedtuple
 
 from ._compat import BytesIO
 from .core import parse_html, pipeline, select, measure, rank, finalise
 
+Extracted = namedtuple('Extracted', 'nodes, tree')
+
+
 def extract(document, encoding='utf-8', count=None):
     if isinstance(document, bytes):
         document = BytesIO(document)
-    
+
     crank = partial(rank, count=count) if count else rank
-    
-    return pipeline(
-        parse_html(document, encoding=encoding),
-        (select, measure, crank, finalise)
-        )
+
+    tree = parse_html(document, encoding=encoding)
+    nodes = pipeline(select(tree), (measure, crank, finalise))
+
+    return Extracted(nodes=nodes, tree=tree)
